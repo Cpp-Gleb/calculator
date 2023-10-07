5dd62650f6b98635b4dfb1494a1fe8d529061924
Cpp-Gleb <115859857+Cpp-Gleb@users.noreply.github.com>
2023-10-08 01:55:33 +0300

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,38 +1,60 @@
 #include <stdio.h>
 int main() 
 {
-	printf("Write number, operation(+,-,*,:), number \n");
 	int a, b;
-	char c;
-	scanf("%d%c%d",&a,&c,&b);
-	switch(c) 
+	char c = ' ';
+	while(c != '!')
 	{
-		case '+':
-			a=a+b;
-			printf("%d",a);
-			break;
-		case '-':
-			a=a-b;
-			printf("%d",a);
-			break;
-		case '*':
-			a=a*b;
-			printf("%d",a);
-			break;
-		case '/':
-			if(b!=0)
-			{
-				a=a/b;
+		printf("Write nomber, operation(+,-,*,/,^), nomber \n");
+		printf("for stop operation write 1!1 \n");
+		scanf("%d%c%d",&a,&c,&b);
+		switch(c) 
+		{
+			case '+':
+				a=a+b;
 				printf("%d",a);
-			}
-			else 
-			{
+				break;
+			case '-':
+				a=a-b;
+				printf("%d",a);
+				break;
+			case '*':
+				a=a*b;
+				printf("%d",a);
+				break;
+			case '/':
+				if(b!=0)
+				{
+					a=a/b;
+					printf("%d",a);
+				}
+				else 
+				{
+					printf("Error!");
+				}
+				break;
+			case '^':
+				int const=a;
+				if(b == 0)
+				{
+					printf("%d",1);
+				}
+				else
+				{
+					for(int i=0;i < b;i++)
+					{
+						a=a*const;
+					}
+					printf("%d",a);
+				}
+			case '!':
+				printf("Thanks for work!");
+				break;
+			default:
 				printf("Error!");
-			}
-			break;
-		default:
-			printf("Error!");
-			break;
+				break;
+		}
+		printf( "\n");
 	}
 	return 0;
 }
